84648bce50faaa290e77a4e7d3be6069600aad3d
Krissada Singhakachain <46844213+OmsinKrissada@users.noreply.github.com>
2024-03-20 13:05:41 +0700

    camelCase

--- Week 7/1_CamelCase.py
+++ Week 7/1_CamelCase.py
@@ -1,11 +1,10 @@
 raw = input()
 transformed = ""
 after_space = False
-for c in raw:
-    if c == " ":
-        after_space = True
-        continue
+for c in "".join([x if x.isalnum() else " " for x in raw]).strip():
     if c.isalnum():
         transformed += c.upper() if after_space else c.lower()
         after_space = False
+    else:
+        after_space = True
 print(transformed)
